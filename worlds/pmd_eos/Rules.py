2e0ddf2826e5214a5b5961320b247cb676b73a8e
Crypticmonkey33 <coolmonkey369@gmail.com>
2025-01-16 00:21:02 -0500

    Added some data into the init function, as well as started building out the Rules

--- worlds/pmd_eos/Rules.py
+++ worlds/pmd_eos/Rules.py
@@ -7,8 +7,14 @@ from .Options import EOSOptions
 if TYPE_CHECKING:
     from . import EOSWorld
 
-def set_rules(world: "EosWorld"):
-    player = world.
-    options = world
 
-    set_rule()
+def ready_for_final_boss():
+    return True
+
+
+def set_rules(world: "EosWorld"):
+    player = world.player
+    options = world.options
+
+    set_rule(world.get_location("Temporal Tower1"),
+             lambda state: ready_for_final_boss())
