48845315f5f7ebd0372ae401a7d44ceab94078aa
Chesyon <55322011+Chesyon@users.noreply.github.com>
2025-01-21 15:43:50 -0500

    fixed a yaml typo

--- worlds/pmd_eos/Options.py
+++ worlds/pmd_eos/Options.py
@@ -4,11 +4,11 @@ from Options import DefaultOnToggle, Range, Toggle, DeathLink, Choice, PerGameCo
 
 
 class DungeonNameRandomizer(DefaultOnToggle):
-    """Randomizes the names of the Dungeons. IDs and completion requirements stay the same"""
+    """Randomizes the names of the dungeons. IDs and completion requirements stay the same"""
     display_name = "Dungeon Name Randomization"
 
 class Goal(Choice):
-    """Cnaged the desired Goal to complete the game"""
+    """Change the desired goal to complete the game"""
     display_name = "Goal"
     option_dialga = 0
     default = 0
