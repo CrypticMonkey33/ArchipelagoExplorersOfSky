8f9e052f65733b4f04e25ef5bbc883caabd06081
Crypticmonkey33 <coolmonkey369@gmail.com>
2025-02-21 16:43:27 -0500

    Added a RomTypeDefinitions.py to define things like the Subx table as necessary

--- worlds/pmd_eos/RomTypeDefinitions.py
+++ worlds/pmd_eos/RomTypeDefinitions.py
@@ -4,12 +4,13 @@ from typing import List, NamedTuple
 class SubXBitfield (NamedTuple):
     bitfield_bit_number: int
     subX_area: int
+    subX_byte: int
     subX_bit_number: int
     flag_definition: str  # name
     prerequisites: List[str]
     default_item: str  # what item is normally there?
 
 subX_table = [
-    SubXBitfield(0, 1, 0, "Bag Upgrade 0", ["Clear Beach Cave"], "Bag Upgrade"),
+    SubXBitfield(0, 1, 0, 0, "Bag Upgrade 0", ["Clear Beach Cave"], "Bag Upgrade"),
 
 ]
