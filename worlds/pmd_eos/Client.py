704835a7369a7fa050ed60e1aa5254b8caa594bf
Crypticmonkey33 <coolmonkey369@gmail.com>
2025-09-01 18:54:32 -0400

    updated the version number

--- worlds/pmd_eos/Client.py
+++ worlds/pmd_eos/Client.py
@@ -18,7 +18,7 @@ from worlds._bizhawk.client import BizHawkClient
 if TYPE_CHECKING:
     from worlds._bizhawk.context import BizHawkClientContext
 
-game_version = "v0.3.1"
+game_version = "v0.3.2prerelease"
 
 
 class EoSClient(BizHawkClient):
